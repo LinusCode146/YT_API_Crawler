2ff9037a1748dc5863a94e4ed8ec48579225c00c
Linus Schulze <linus.schulze@denkmit.de>
2023-03-14 23:13:18 +0100

    added git

--- constances.py
+++
@@ -1,8 +0,0 @@
-API_KEY = "AIzaSyBMfR0oKnFZCUiSobQKtXjwdvStIMhoPPk"
-CHANNEL_IDS = [
-    "UCQHX6ViZmPsWiYSFAyS0a3Q",  # GothamChess
-    "UCIabPXjvT5BVTxRDPCBBOOQ",  # DaniDev
-    "UC4JX40jDee_tINbkjycV4Sg",  # TwT
-    "UClb90NQQcskPUGDIXsQEz5Q",  # Dev ed
-    "UCrPseYLGpNygVi34QpGNqpA",  # Ludwig
-]
